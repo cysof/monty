1464ba5916218f670647688636348c28e7e461d0
cysof <cp4reallife@yahoo.com>
2023-11-17 21:33:30 -0800

    task 3-5

---
+++ _nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - Does nothing.
+ * @head: Pointer to the head of the stack.
+ * @number: Line number in the bytecode file.
+ */
+void f_nop(stack_t **head, unsigned int number)
+{
+	(void)head;
+	(void)number;
+}
+
